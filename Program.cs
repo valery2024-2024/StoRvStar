d06f78febda34ffe10e1568dbb7ff04ee59cdf21
FORMANJUK VALERIJ <sof20b13@gmail.com>
2026-05-12 21:50:30 +0300

    Harden identity settings and wire configurable seed options

--- Program.cs
+++ Program.cs
@@ -14,15 +14,23 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=storvstar.db"));
 
+// Identity seed options
+builder.Services.Configure<IdentitySeedOptions>(
+    builder.Configuration.GetSection(IdentitySeedOptions.SectionName));
+
 // Identity (ОСНОВА АВТОРИЗАЦІЇ)
 builder.Services
     .AddIdentity<AppUser, IdentityRole>(options =>
     {
-        options.Password.RequireDigit = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequiredLength = 6;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequiredLength = 8;
+
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
     })
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
@@ -39,9 +47,10 @@ builder.Services.AddScoped<IServiceRequestService, ServiceRequestService>();
 
 var app = builder.Build();
 
-// СТВОРЕННЯ КОРИСТУВАЧІВ І РОЛЕЙ
-using (var scope = app.Services.CreateScope())
+// СТВОРЕННЯ РОЛЕЙ І DEV-АДМІНА
+if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
     var services = scope.ServiceProvider;
     await IdentitySeeder.SeedAsync(services);
 }
